b3e629e65b3be751339831b793dfb53635b3ef2f
MrSWagner <mr.wagnersa@gmail.com>
2020-02-26 22:32:23 -0500

    added multiple dice compatibility

--- Dice.py
+++ Dice.py
@@ -3,11 +3,21 @@ class Dice:
 
     
     #Initializes the Dice object with a number of faces, stored as DieSize
-    def __init__(self, DieSize):
+    def __init__(self, DieSize = 20):
         self.DieSize = DieSize
 
 
     #Rolls the Dice object, returning a value between 1 and the DieSize
-    def Roll(self):
-        theRoll = random.randrange(1,(self.DieSize+1))
+    def Roll(self, numDie = 1):
+        i = 0
+        theRoll = 0
+        while i < numDie:
+            if theRoll == 0:
+                theRoll = random.randrange(1,(self.DieSize+1))
+            else:
+                theRoll += random.randrange(1,(self.DieSize+1))
+            i += 1
         return theRoll
+    
+    def getSize(self):
+        return self.DieSize
