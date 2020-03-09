5f07cdfbc76cf2ace4120b323bbf1664ea59f326
MrSWagner <mr.wagnersa@gmail.com>
2020-03-08 21:46:50 -0400

    Reads monsters from JSON file
    Entity creation is in a new class EntityFactory
    Monsters.JSON has three monsters in it, future updates should increase
    this number to include all monsters
    Entity now deals only with things entities can "do"
    EntityFactory now creates all entities

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import Analysis
 import Entity
+import EntityFactory
 
 #List of accepted values to indicate user means yes
 YesValues = ["Y", "YEAH", "YUP", "AFFIRMATIVE", "YES", "CORRECT", "RIGHT"]
@@ -9,15 +10,19 @@ ContFlag = "Y"
 
 #While the user wants to continue, keep running new scenarios
 while ContFlag in YesValues:
-    PC = Entity.Entity.buildEntity()
-    SC = Entity.Entity("Skeletal Champion", 2, 25, 19, 8, 8, 10, 5, 4)
-    print("For this Beta Version, you will fight a set creature, a Skeletal Champion.")
+    Make = EntityFactory.EntityFactory("Monsters.JSON")
+    PC = EntityFactory.EntityFactory.buildNewPCFromUser()
+    SC = Make.BuildEntityFromJSON()
+    
     #Create the Analysis object
     Battle = Analysis.Analysis(PC, SC)
     
     #Give the probabilities and values to beat
     Battle.AnalyzeToHit()
-      
+    
+    #Has the entities fight to the death
+    #Prints out a play by play and declares the winner
     PC.CharactersFightToDeath(SC)
-            
+    
+    #Checks if the user would like to stop     
     ContFlag = str(input("Would you like to run the simulator again?")).upper()
